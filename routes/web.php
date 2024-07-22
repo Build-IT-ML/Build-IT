ec5dc4f9ce6df4c66e2b3e90ef2f0f4f281cf680
KadekDwiki <nusanjayadwiki@gmail.com>
2024-07-22 16:17:22 +0800

    add: spatie laravel permission

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,27 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render("Welcome", []);
 })->name('Welcome');
+
+Route::get('/modul/alprog', function () {
+    return Inertia::render("Modul/Alprog", []);
+})->name('Modul/Alprog');
+
+Route::get('/modul/basisdata', function () {
+    return Inertia::render("Modul/BasisData", []);
+})->name('Modul/BasisData');
+
+Route::get('/modul/jarkom', function () {
+    return Inertia::render("Modul/Jarkom", []);
+})->name('Modul/Jarkom');
+
+Route::get('/login', function () {
+    return Inertia::render("Login", []);
+})->name('Login');
+
+Route::get('/register', function () {
+    return Inertia::render("Daftar", []);
+})->name('Daftar');
+
+Route::fallback(function () {
+    return Inertia::render('not-found');
+})->name('not-found');
