a8e71af69a0ca92cee6284d84579adab3950f082
KadekDwiki <nusanjayadwiki@gmail.com>
2024-08-06 00:39:44 +0800

    update: participant can add submission when verification status is Terverifikasi

--- resources/js/Pages/Participant/Submission.jsx
+++ resources/js/Pages/Participant/Submission.jsx
@@ -21,70 +21,79 @@ export default function Submission() {
    return(
       <AdminAuthentication user={user} headerTitle="Pengumpulan Tugas Peserta">
          <Head title="Participant Submission"/>
-         <div className="space-y-12">
-            <form onSubmit={submit} className="space-y-5">
-               <div className="flex flex-col gap-2 w-full">
-                  <h3 className="text-xl text-primary font-bold">Link Pengumpulan Tugas Algoritma & Pemrograman</h3>
-                  <label htmlFor='alprog' className="text-primary/80 text-base">Link Google Drive</label>
-                  <div className="space-x-4">
-                     <input 
-                        type="text" 
-                        name="alprog"
-                        id="alprog" 
-                        value={data.alprog}
-                        onChange={(e) => setData('alprog', e.target.value)}
-                        className={`bg-secondary/20 w-1/2 border-2 rounded-lg focus:outline-none focus:ring-1  text-primary p-2 ${errors.nim ? "border-red-500 focus:ring-red-500" : "border-primary focus:ring-primary"}`}
-                     />
-                     <button type="submit" className="py-3 px-6 bg-primary hover:bg-secondary font-bold text-white rounded-lg transition-all duration-300 text-center">
-                        Submit
-                     </button>
-                  </div>
-                  <small className="text-red-500">
-                     {errors.alprog}
-                  </small>
+            {user.status === 'Belum Terverifikasi' || user.status === 'Ditolak' && (
+               <div className="flex flex-col justify-center items-center space-y-10 min-h-screen">
+                  <h1 className="font-bold text-red-500 text-xl">Halaman pengumpulan tugas tersedia jika status peserta sudah terverifikasi</h1>
+                  <span><i className="pi pi-exclamation-circle text-[10rem] text-red-500"></i></span>
                </div>
-               <div className="flex flex-col gap-2 w-full">
-                  <h3 className="text-xl text-primary font-bold">Link Pengumpulan Tugas Basis Data</h3>
-                  <label htmlFor='basisdata' className="text-primary/80 text-base">Link Google Drive</label>
-                  <div className="space-x-4">
-                     <input 
-                        type="text" 
-                        name="basisdata"
-                        id="basisdata" 
-                        value={data.basisdata}
-                        onChange={(e) => setData('basisdata', e.target.value)}
-                        className={`bg-secondary/20 w-1/2 border-2 rounded-lg focus:outline-none focus:ring-1  text-primary p-2 ${errors.nim ? "border-red-500 focus:ring-red-500" : "border-primary focus:ring-primary"}`}
-                     />
-                     <button type="submit" className="py-3 px-6 bg-primary hover:bg-secondary font-bold text-white rounded-lg transition-all duration-300 text-center">
-                        Submit
-                     </button>
-                  </div>
-                  <small className="text-red-500">
-                     {errors.basisdata}
-                  </small>
+            ) }
+
+            {user.status === 'Terverifikasi' && (
+               <div className="space-y-12">
+                  <form onSubmit={submit} className="space-y-5">
+                     <div className="flex flex-col gap-2 w-full">
+                        <h3 className="text-xl text-primary font-bold">Link Pengumpulan Tugas Algoritma & Pemrograman</h3>
+                        <label htmlFor='alprog' className="text-primary/80 text-base">Link Google Drive</label>
+                        <div className="space-x-4">
+                           <input 
+                              type="text" 
+                              name="alprog"
+                              id="alprog" 
+                              value={data.alprog}
+                              onChange={(e) => setData('alprog', e.target.value)}
+                              className={`bg-secondary/20 w-1/2 border-2 rounded-lg focus:outline-none focus:ring-1  text-primary p-2 ${errors.nim ? "border-red-500 focus:ring-red-500" : "border-primary focus:ring-primary"}`}
+                           />
+                           <button type="submit" className="py-3 px-6 bg-primary hover:bg-secondary font-bold text-white rounded-lg transition-all duration-300 text-center">
+                              Submit
+                           </button>
+                        </div>
+                        <small className="text-red-500">
+                           {errors.alprog}
+                        </small>
+                     </div>
+                     <div className="flex flex-col gap-2 w-full">
+                        <h3 className="text-xl text-primary font-bold">Link Pengumpulan Tugas Basis Data</h3>
+                        <label htmlFor='basisdata' className="text-primary/80 text-base">Link Google Drive</label>
+                        <div className="space-x-4">
+                           <input 
+                              type="text" 
+                              name="basisdata"
+                              id="basisdata" 
+                              value={data.basisdata}
+                              onChange={(e) => setData('basisdata', e.target.value)}
+                              className={`bg-secondary/20 w-1/2 border-2 rounded-lg focus:outline-none focus:ring-1  text-primary p-2 ${errors.nim ? "border-red-500 focus:ring-red-500" : "border-primary focus:ring-primary"}`}
+                           />
+                           <button type="submit" className="py-3 px-6 bg-primary hover:bg-secondary font-bold text-white rounded-lg transition-all duration-300 text-center">
+                              Submit
+                           </button>
+                        </div>
+                        <small className="text-red-500">
+                           {errors.basisdata}
+                        </small>
+                     </div>
+                     <div className="flex flex-col gap-2 w-full">
+                        <h3 className="text-xl text-primary font-bold">Link Pengumpulan Tugas Jaringan dan Komputer</h3>
+                        <label htmlFor='jarkom' className="text-primary/80 text-base">Link Google Drive</label>
+                        <div className="space-x-4">
+                           <input 
+                              type="text" 
+                              name="jarkom"
+                              id="jarkom" 
+                              value={data.jarkom}
+                              onChange={(e) => setData('jarkom', e.target.value)}
+                              className={`bg-secondary/20 w-1/2 border-2 rounded-lg focus:outline-none focus:ring-1  text-primary p-2 ${errors.nim ? "border-red-500 focus:ring-red-500" : "border-primary focus:ring-primary"}`}
+                           />
+                           <button type="submit" className="py-3 px-6 bg-primary hover:bg-secondary font-bold text-white rounded-lg transition-all duration-300 text-center">
+                              Submit
+                           </button>
+                        </div>
+                        <small className="text-red-500">
+                           {errors.jarkom}
+                        </small>
+                     </div>
+                  </form>   
                </div>
-               <div className="flex flex-col gap-2 w-full">
-                  <h3 className="text-xl text-primary font-bold">Link Pengumpulan Tugas Jaringan dan Komputer</h3>
-                  <label htmlFor='jarkom' className="text-primary/80 text-base">Link Google Drive</label>
-                  <div className="space-x-4">
-                     <input 
-                        type="text" 
-                        name="jarkom"
-                        id="jarkom" 
-                        value={data.jarkom}
-                        onChange={(e) => setData('jarkom', e.target.value)}
-                        className={`bg-secondary/20 w-1/2 border-2 rounded-lg focus:outline-none focus:ring-1  text-primary p-2 ${errors.nim ? "border-red-500 focus:ring-red-500" : "border-primary focus:ring-primary"}`}
-                     />
-                     <button type="submit" className="py-3 px-6 bg-primary hover:bg-secondary font-bold text-white rounded-lg transition-all duration-300 text-center">
-                        Submit
-                     </button>
-                  </div>
-                  <small className="text-red-500">
-                     {errors.jarkom}
-                  </small>
-               </div>
-            </form>   
-         </div>
+            )}
       </AdminAuthentication>
    )
 }
