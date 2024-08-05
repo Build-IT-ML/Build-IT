2055e3cb9e7112282492d56c4bc86c60d10c8580
KadekDwiki <nusanjayadwiki@gmail.com>
2024-08-06 00:05:13 +0800

    edit: group middleware for guest

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ParticipantProfileController;
 use App\Http\Controllers\ParticipantSubmissionController;
+use App\Http\Controllers\AdminSubmissionsController;
 use GuzzleHttp\Middleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
@@ -23,34 +24,38 @@ use Inertia\Inertia;
 */
 
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
-Route::get('/participant-profile', [ParticipantProfileController::class, 'index'])->middleware(['auth'])->name('participant.profile');
-Route::get('/participant-submissions', [ParticipantSubmissionController::class, 'index'])->middleware(['auth'])->name('participant.submissions');
-Route::put('/participant-submissions', [ParticipantSubmissionController::class, 'update'])->middleware(['auth'])->name('participant.addsubmissions');
-Route::resource('participants', AdminParticipantsController::class)->middleware(['auth']);
+Route::get('/', function () {
+    return Inertia::render("Welcome", []);
+})->name('Welcome');
+
+Route::get('/modul/alprog', function () {
+    return Inertia::render("Modul/Alprog", []);
+})->name('Modul.Alprog');
+
+Route::get('/modul/basisdata', function () {
+    return Inertia::render("Modul/BasisData", []);
+})->name('Modul.BasisData');
+
+Route::get('/modul/jarkom', function () {
+    return Inertia::render("Modul/Jarkom", []);
+})->name('Modul.Jarkom');
+
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
-Route::middleware('guest')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render("Welcome", []);
-    })->name('Welcome');
+    Route::middleware('participant')->group(function () {
+        Route::get('/profile', [ParticipantProfileController::class, 'index'])->middleware(['auth'])->name('participant.profile');
+        Route::get('/edit-profile', [ParticipantProfileController::class, 'edit'])->middleware(['auth'])->name('participant.editprofile');
+        Route::put('/edit-profile', [ParticipantProfileController::class, 'update'])->middleware(['auth'])->name('participant.updateprofile');
+        Route::get('/participant-submissions', [ParticipantSubmissionController::class, 'index'])->middleware(['auth'])->name('participant.submissions');
+        Route::put('/participant-submissions', [ParticipantSubmissionController::class, 'update'])->middleware(['auth'])->name('participant.addsubmissions');
+    });
 
-    Route::get('/modul/alprog', function () {
-        return Inertia::render("Modul/Alprog", []);
-    })->name('Modul.Alprog');
-
-    Route::get('/modul/basisdata', function () {
-        return Inertia::render("Modul/BasisData", []);
-    })->name('Modul.BasisData');
-
-    Route::get('/modul/jarkom', function () {
-        return Inertia::render("Modul/Jarkom", []);
-    })->name('Modul.Jarkom');
+    Route::middleware('admin')->group(function () {
+        Route::resource('participants', AdminParticipantsController::class)->middleware(['auth']);
+        Route::resource('submissions', AdminSubmissionsController::class)->middleware(['auth']);
+    });
 });
 
 Route::fallback(function () {
