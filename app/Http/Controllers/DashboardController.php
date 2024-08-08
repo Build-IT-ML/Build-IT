e5ac99165f6a0ac200b326f8ecdfd29adfb8022a
KadekDwiki <nusanjayadwiki@gmail.com>
2024-08-08 11:49:02 +0800

    update: count if status Terverifikasi

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,10 +12,10 @@ class DashboardController extends Controller
     {
         $user = auth()->user();
         $role = auth()->user()->getRoleNames();
-        $akt21 = User::where('nim', 'like', '21%')->count();
-        $akt22 = User::where('nim', 'like', '22%')->count();
-        $akt23 = User::where('nim', 'like', '23%')->count();
-        $akt24 = User::where('nim', 'like', '24%')->count();
+        $akt21 = User::where('nim', 'like', '21%')->where('status', 'Terverifikasi')->count();
+        $akt22 = User::where('nim', 'like', '22%')->where('status', 'Terverifikasi')->count();
+        $akt23 = User::where('nim', 'like', '23%')->where('status', 'Terverifikasi')->count();
+        $akt24 = User::where('nim', 'like', '24%')->where('status', 'Terverifikasi')->count();
 
 
         return Inertia::render('Dashboard', compact('user', 'role', 'akt21', 'akt22', 'akt23', 'akt24'));
