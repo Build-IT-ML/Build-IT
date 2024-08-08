7573d013f6195edbfb339c9a4bc9330894b1c3c1
KadekDwiki <nusanjayadwiki@gmail.com>
2024-08-08 11:50:02 +0800

    update: ignore UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,44 +21,9 @@ class DatabaseSeeder extends Seeder
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'participant']);
 
-
-        $user = User::create([
-            'id' =>  $uuid->toString(),
-            'nim' => 'admin1',
-            'name' => 'admin1',
-            'email' => 'admin1@gmail.com',
-            'line_id' => 'admin1',
-            'whatsapp_id' => 'admin1',
-            'password' => "password"
+        $this->call([
+            UserSeeder::class
         ]);
-
-        $user->assignRole('admin');
-
-        $user = User::create([
-            'id' =>  $uuid->toString(),
-            'nim' => 'admin2',
-            'name' => 'admin2',
-            'email' => 'admin2@gmail.com',
-            'line_id' => 'admin2',
-            'whatsapp_id' => 'admin2',
-            'password' => "password"
-        ]);
-
-        $user->assignRole('admin');
-
-        $user = User::create([
-            'id' =>  $uuid->toString(),
-            'nim' => 'pengguna1',
-            'name' => 'pengguna1',
-            'email' => 'pengguna1@gmail.com',
-            'line_id' => 'pengguna1',
-            'whatsapp_id' => 'pengguna1',
-            'password' => "password",
-            'kelompok' => '2',
-            'status' => 'Terverifikasi'
-        ]);
-
-        $user->assignRole('participant');
 
         User::factory(150)->create()->each(function ($users) {
             $users->assignRole('participant');
