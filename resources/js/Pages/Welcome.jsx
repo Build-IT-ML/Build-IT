fed8a300beba188efe220c33979a3b4621c79c01
PutuRifki <puturifki0@gmail.com>
2024-08-12 15:36:26 +0800

    update

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -624,7 +624,7 @@ const CardMatkul = ({ heading, description, button, image, url }) => {
                 </p>
                 <button 
                     onClick={toggleDescription} 
-                    className="text-white font-bold text-[14px] mt-2 leading-[0.05em]">
+                    className="text-white font-bold text-[14px] mt-2 leading-[0.05em] hover:underline">
                     {bacaSelengkapnya ? 'Tampilkan Lebih Sedikit' : 'Baca Selengkapnya'}
                 </button>
                 <a href={url} className="text-primary font-bold text-[14px] font-bold leading-[24px] tracking-[0.2px]">
