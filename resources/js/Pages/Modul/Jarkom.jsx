35df45fa3bfb23497729818625437caa1a8de489
PutuRifki <puturifki0@gmail.com>
2024-08-12 11:53:36 +0800

    restyle: modul page

--- resources/js/Pages/Modul/Jarkom.jsx
+++ resources/js/Pages/Modul/Jarkom.jsx
@@ -76,7 +76,7 @@ export default function JaringanKomputer() {
                             </div>
                             <div className="w-full mt-20 flex justify-center mb-36">
                                 <img src="../../../asset/images/modul/jarkom.JPG" alt="test" 
-                                className="w-[441px] h-[248px] rounded-[10px] relative right-0 object-cover z-20"/>
+                                className="w-[441px] h-[248px] rounded-[20px] relative right-0 object-cover z-20"/>
                             </div>
                         </div>
                     </div>
